53e73743a7f261845d88e8b5d430e90fa46d88cd
Eduardo Garza <125391180+eduardogarzab@users.noreply.github.com>
2025-02-22 20:28:41 -0600

    Production Build

--- components/theme-provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const NextThemesProvider = dynamic(() => import("next-themes").then((mod) => mod.ThemeProvider), { ssr: false });
-
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
-	return <NextThemesProvider>{children}</NextThemesProvider>;
-}
